7b329c90052a325b3bb787d674626f073988f177
Juns1102 <wnstj1302@naver.com>
2024-11-23 17:26:58 +0900

    commit

--- Assets/Scripts/Manager/InventoryManager.cs
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -5,4 +5,17 @@ using System.Collections.Generic;
 
 public class InventoryManager : Singleton<InventoryManager> {
     public List<GameObject> items = new List<GameObject>();
+
+
+    private void SortName() {
+
+    }
+
+    private void SortValue() {
+
+    }
+
+    private void SortWeight() {
+
+    }
 }
