95c5d8f7503b2455a17b80812066e837af43a089
Juns1102 <wnstj1302@naver.com>
2024-11-27 03:21:28 +0900

    commit

--- Assets/Scripts/Manager/InventoryManager.cs
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -9,7 +9,7 @@ public class InventoryManager : Singleton<InventoryManager> {
     public int currentCapacity;
     public List<ItemData> items = new List<ItemData>();
 
-    private void Awake() {
+    private void Start() {
         currentCapacity = 0;
     }
 
@@ -144,22 +144,5 @@ public class InventoryManager : Singleton<InventoryManager> {
             currentCapacity -= items[num].weight;
             items.RemoveAt(num);
         }
-    }
-
-    private void Swap(List<GameObject> list, int a, int b)
-    {
-        GameObject temp = list[a];
-        list[a] = list[b];
-        list[b] = temp;
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) SortName();
-        if (Input.GetKeyDown(KeyCode.Alpha2)) SortValue();
-        if (Input.GetKeyDown(KeyCode.Alpha3)) SortWeight();
-        if (Input.GetKeyDown(KeyCode.Alpha5)) ReverseSortName();
-        if (Input.GetKeyDown(KeyCode.Alpha6)) ReverseSortValue();
-        if (Input.GetKeyDown(KeyCode.Alpha7)) ReverseSortWeight();
     }
 }
