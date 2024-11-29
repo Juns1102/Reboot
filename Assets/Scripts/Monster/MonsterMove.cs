3019de751170fb30e1e300b5be55ccf81ea87116
Succession-Kyeom <kim1031sk@naver.com>
2024-11-29 14:24:16 +0900

    작업중222
    dfs로 길찾기

--- Assets/Scripts/Monster/MonsterMove.cs
+++ Assets/Scripts/Monster/MonsterMove.cs
@@ -2,84 +2,79 @@ using NUnit.Framework;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 public class MonsterMove : MonoBehaviour {
     const int INF = 10000;
     Vector2 playerPos; //플레이어 좌표
     Vector2 myPos; //몬스터 좌표
-    RaycastHit2D hit;
-    Vector2[][] path;
-    int[][] weight;
+    int[][] weight; //가중치 배열
+    
 
     //먼저 주변 맵 정보를 수집
     //수집한 정보를 토대로 길 찾기
     //최소 경로 추적
 
+    //플레이어, 몬스터 사이 범위 맵 정보 수집
     void GetMapInfo() {
-        RaycastHit2D hit = Physics2D.Raycast(myPos, myPos, LayerMask.GetMask("Platform"));
-        if(myPos.x <= playerPos.x) {
-            if(myPos.y <= playerPos.y) {
-                for(int i = 0; i < playerPos.x - myPos.x; i++) {
-                    //if(Physics2D.Raycast(myPos + new Vector2(i, 0), 1))
+        Array.Clear(weight, 0, weight.Length); //가중치 배열 초기화(움직일 때마다 범위, 값 바껴서 초기화 해줘야함)
+        float xDistance = Mathf.Abs(myPos.x - playerPos.x); //플레이어 - 몬스터 x거리
+        float yDistance = Mathf.Abs(myPos.y - playerPos.y); //플레이어 - 몬스터 y거리
+        RaycastHit2D hit; //레이어 확인 변수
+
+        for(int i = 0; i < xDistance; i++) {
+            for(int j = 0; j < yDistance; j++) {
+                hit = Physics2D.Raycast(myPos, myPos, LayerMask.GetMask("Platform")); //현재 위치 레이어 확인
+                if(hit.collider == null) { //평지(가중치 1)일 경우
+                    weight[i][j] = 1;
+                }
+                else { //장애물(가중치 INF) 존재할 경우
+                    weight[i][j] = INF;
                 }
             }
         }
+    }
+
+    int MoveHorizontal(int x, int minWeight) {
+        //가중치 합 < 최소라면 ㄱㄱ
+        if(x < playerPos.x) {
+            MoveHorizontal(x + 1, minWeight);
+        }
+        else if(x > playerPos.x) {
+            MoveHorizontal(x - 1, minWeight);
+        }
+
+        return weight[x][x]; //임시 반환
+    }
+
+    void MoveVertical(int y, int minWeight) {
+        if(y < playerPos.y) {
+        
+        }
+        else if(y > playerPos.y) {
+
+        }
         else {
-
+            return;
         }
     }
 
-	void Chasing() {
-        int i, u, w;
-
-        for(i = 0; i < Mathf.Abs(myPos.x - playerPos.x) + Mathf.Abs(myPos.y - playerPos.y); i++) {
-
-        }
-
-        //간선 개수 == (몬스터x  - 플레이어x) + (몬스터y - 플레이어y)
-        for(i = 0; i < Mathf.Abs(myPos.x - playerPos.x) + Mathf.Abs(myPos.y - playerPos.y); i++) {
-            
-        }
-	}
-
-	/*
-    void shortest_path(GraphType* g, int start) {
-        int i, u, w;
-        for (i = 0; i<g->n; i++) { 초기화
-            distance[i] = g->weight[start][i];
-            found[i] = FALSE;
-        }
-        found[start] = TRUE;    시작 정점 방문 표시
-        distance[start] = 0;
-        for (i = 0; i < g->n - 1; i++) {
-	        print_status(g);
-	        u = choose(distance, g->n, found);
-	        found[u] = TRUE;
-	        for (w = 0; w < g->n; w++)
-		        if (!found[w])
-        			if (distance[u] + g->weight[u][w] < distance[w])
-				        distance[w] = distance[u] + g->weight[u][w];
-        }
+    void Dfs() {
+        MoveHorizontal((int)myPos.x, INF);
+        MoveVertical((int)myPos.y, INF);
     }
-    */
+
+    Vector2 Chasing() {
+        //초기화
+        GetMapInfo();
+
+        Dfs();
+
+        return myPos; //임시 반환
+    }
 
 	void Wandering() {
-        /*
-        switch(Random.Range(0, 4)) {
-            case 0: //상
-				hit = Physics2D.Raycast(myPos, transform.forward, 1, LayerMask.GetMask("Platform"));
-				break;
-            case 1: //하
-				hit = Physics2D.Raycast(myPos, -transform.forward, 1, LayerMask.GetMask("Platform"));
-				break;
-            case 2: //좌
-				hit = Physics2D.Raycast(myPos, transform.right, 1, LayerMask.GetMask("Platform"));
-				break;
-            case 3: //우
-				hit = Physics2D.Raycast(myPos, -transform.right, 1, LayerMask.GetMask("Platform"));
-				break;
-        }
-        */
     }
 
     void Move() {
@@ -97,10 +92,4 @@ public class MonsterMove : MonoBehaviour {
         playerPos = GameObject.Find("TestPlayer").transform.position;
         myPos = this.transform.position;
     }
-
-    // Update is called once per frame
-    void Update() {
-		RaycastHit2D hit = Physics2D.Raycast(myPos, new Vector2(0, 0), LayerMask.GetMask("Platform"));
-        Debug.Log(hit.collider);
-	}
 }
